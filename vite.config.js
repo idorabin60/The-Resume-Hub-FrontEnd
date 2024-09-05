c0a35ca3da189eff98d30d0ce2e102e430a2072c
idorabin60 <idorabin60@gmail.com>
2024-09-05 19:20:22 +0300

    done setup

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  css: {
-    postcss: "./postcss.config.js", // Ensure Vite is using the correct PostCSS config
-  },
 });
